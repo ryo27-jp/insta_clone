82bb8073c1345c7ba2df0847166a354a454fac5b
ryo27-jp <calvee.20@gmail.com>
2020-11-29 19:40:31 +0900

    post機能のルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'users#new'
 
   resources :users, only: %i[new create]
+  resources :posts
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
