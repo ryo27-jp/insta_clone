72729ea62d94605b45be7a8530aef33e8390929b
ryo27-jp <calvee.20@gmail.com>
2020-12-01 16:28:12 +0900

    swiper.jsを最後に読み込む様に修正

--- app/assets/javascripts/swiper.js
+++ app/assets/javascripts/swiper.js
@@ -1,5 +1,8 @@
-var swiper = new Swiper('.swiper-container', {
-  pagination: {
-    el: '.swiper-pagination',
-  },
-});
+// jQueryの関数で予約状態としてHTMLの読み込みが終了したら実行される。
+$(function() {
+  new Swiper('.swiper-container', {
+      pagination: {
+          el: '.swiper-pagination',
+      },
+  })
+})
