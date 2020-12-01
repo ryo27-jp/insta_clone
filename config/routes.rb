bacb56e3a22c41f3a6346e680767c612046ad213
ryo27-jp <calvee.20@gmail.com>
2020-12-01 17:47:49 +0900

    ルートパス変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  root 'posts#index'
 
   resources :users, only: %i[new create]
   resources :posts
-  get 'user_sessions/new'
+
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
