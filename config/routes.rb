0d4b1083d2fc448ea354750f3b16f51cf67cb161
ryo27-jp <calvee.20@gmail.com>
2020-12-03 23:06:23 +0900

    細かな修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   resources :users, only: %i[new create]
-<<<<<<< HEAD
   resources :posts
 
-=======
->>>>>>> parent of 82bb807... post機能のルーティング作成
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
