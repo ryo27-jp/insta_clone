e764cd00ea7165a1531000feda6fad189898672d
ryo27-jp <calvee.20@gmail.com>
2020-12-12 18:24:52 +0900

    validation修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -23,5 +23,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :body, presence: true, length: { maximum :280 }
+  validates :body, presence: true, length: { maximum: 280 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -24,5 +24,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  validates :body, presence: true, length: { maximum :280 }
+  validates :body, presence: true, length: { maximum: 280 }
 end
