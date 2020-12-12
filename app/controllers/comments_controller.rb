e2bbfd413cc6ca1b9aba11796665695e6ed729c1
ryo27-jp <calvee.20@gmail.com>
2020-12-12 23:14:38 +0900

    commentのCRUD作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,17 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    @comment = current_user.comments.find(params[:id])
   end
 
   def update
+    @comment = current_user.comments.find(params[:id])
+    @comment.update(comment_update_params)
   end
 
   def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
   end
 
   private
@@ -18,5 +23,9 @@ class CommentsController < ApplicationController
     def comment_params
       # mergeメソッドでルーティングでネストされているpost_idを結合している。
       params.require(:comment).permit(:body).merge(post_id: params[:post_id])
+    end
+    
+    def comment_update_params
+      params.require(:comment).permit(:body)
     end
 end
