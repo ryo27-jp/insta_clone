05eab54ce566c2311fd66cc3aeeedb185580a4e9
ryo27-jp <calvee.20@gmail.com>
2021-01-13 11:06:13 +0900

    フォロー機能のルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   resources :likes, only: %i[create destroy]
+  resources :relationships, only: %i[create destroy]
   resources :users, only: %i[new create]
   resources :posts do
     # コレクションだけを親のスコープの下で生成。メンバーはネストに含めない。
