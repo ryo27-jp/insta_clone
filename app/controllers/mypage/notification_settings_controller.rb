accc1afdc4759a7d1695e89102d3e2605c9c868f
ryo27-jp <calvee.20@gmail.com>
2021-02-18 12:02:52 +0900

    通知設定のViewを作成

--- app/controllers/mypage/notification_settings_controller.rb
+++ app/controllers/mypage/notification_settings_controller.rb
@@ -7,7 +7,7 @@ class Mypage::NotificationSettingsController < Mypage::BaseController
   def update
     @user = User.find(current_user)
     if @user.update(notification_settings_params)
-      redirect_to edit_mypage_notification_setting_path, success '設定を更新しました'
+      redirect_to edit_mypage_notification_setting_path, success: '設定を更新しました'
     else
       flash.now[:danger] = "設定の更新に失敗しました"
       render edit_mypage_notification_setting_path
